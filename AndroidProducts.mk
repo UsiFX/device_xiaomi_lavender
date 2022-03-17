01c9f9b2bb7d611913e63e1e53fff0b3d5d6d89b
UsiFX <xprjkts@gmail.com>
2022-03-18 01:05:33 +0200

    lavender: dotOS bringup
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_lavender.mk
+    $(LOCAL_DIR)/dot_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_lavender-user \
-    revengeos_lavender-userdebug \
-    revengeos_lavender-eng
+    dot_lavender-user \
+    dot_lavender-userdebug \
+    dot_lavender-eng
