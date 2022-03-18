4452e038d03c0ef5db7173ff356b1eb9b500a4d8
UsiFX <xprjkts@gmail.com>
2022-03-18 20:42:52 +0200

    lavender: materium bringup
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_lavender.mk
+    $(LOCAL_DIR)/materium_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_lavender-user \
-    revengeos_lavender-userdebug \
-    revengeos_lavender-eng
+    materium_lavender-user \
+    materium_lavender-userdebug \
+    materium_lavender-eng
